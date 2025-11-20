38d3bb3b7eddf18980d7fa532f195a72cedfd821
Vicente Junior <vicente_pc@outlook.com>
2025-11-20 12:15:17 -0300

    ajustes de classes, implementação de CombustivelApiService (API externa de Preços)

---
+++ AutoManeger.Dominio/ModuloPrecoCombustivel/IRepositorioPrecoCombustivel.cs
@@ -0,0 +1,5 @@
+﻿using AutoManager.Dominio.Compartilhado;
+
+namespace AutoManager.Dominio.ModuloPrecoCombustivel;
+
+public interface IRepositorioPrecoCombustivel : IRepositorio<PrecoCombustivel>;
---
+++ AutoManeger.Dominio/ModuloPrecoCombustivel/PrecoCombustivel.cs
@@ -0,0 +1,30 @@
+﻿using AutoManager.Dominio.Compartilhado;
+
+namespace AutoManager.Dominio.ModuloPrecoCombustivel;
+
+public class PrecoCombustivel : EntidadeBase<PrecoCombustivel>
+{
+    public string Estado { get; set; }
+    public string TipoCombustivel { get; set; }
+    public decimal PrecoMedio { get; set; }
+    public DateTime DataAtualizacao { get; set; }
+
+    public PrecoCombustivel() { }
+
+    public PrecoCombustivel(string estado, string tipoCombustivel, decimal precoMedio, DateTime dataAtualizacao)
+    {
+        Estado = estado;
+        TipoCombustivel = tipoCombustivel;
+        PrecoMedio = precoMedio;
+        DataAtualizacao = dataAtualizacao;
+    }
+
+    public override void AtualizarRegistro(PrecoCombustivel registroAtualizado)
+    {
+        Estado = registroAtualizado.Estado;
+        TipoCombustivel = registroAtualizado.TipoCombustivel;
+        PrecoMedio = registroAtualizado.PrecoMedio;
+        DataAtualizacao = registroAtualizado.DataAtualizacao;
+    }
+}
+
