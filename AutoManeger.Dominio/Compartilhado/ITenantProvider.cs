5bc2a0cfa128e4094b57c9d9ea2b3d6ec9588f95
Vicente Junior <vicente_pc@outlook.com>
2025-11-21 11:14:43 -0300

    add ItenantProvider no dominio

---
+++ AutoManeger.Dominio/Compartilhado/ITenantProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoManager.Dominio.Compartilhado;
+
+public interface ITenantProvider
+{
+    Guid? UsuarioId { get; }
+    bool IsInRole(string roleName);
+}
