e7a7e0f1b370cadbe1a31bb48133009952393013
Vicente Junior <vicente_pc@outlook.com>
2025-11-22 15:18:01 -0300

    add repositorioTaxaServico

---
+++ AutoManager.Infraestrutura.Orm/ModuloTaxaServico/RepositorioTaxaServicoEmOrm.cs
@@ -0,0 +1,25 @@
+﻿using AutoManager.Dominio.ModuloTaxaServico;
+using AutoManager.Infraestrutura.Orm.Compartilhado;
+using AutoManager.Infraestrutura.Orm.Compatilhado;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoManager.Infraestrutura.Orm.ModuloTaxaServico;
+
+public class RepositorioTaxaServicoEmOrm : RepositorioBaseEmOrm<TaxaServico>, IRepositorioTaxaServico
+{
+    public RepositorioTaxaServicoEmOrm(AutoManagerDbContext dbContext) : base(dbContext) { }
+
+    public override TaxaServico? SelecionarPorId(Guid idRegistro)
+    {
+        return dbSet
+            .Include(t => t.Empresa)
+            .FirstOrDefault(t => t.Id == idRegistro);
+    }
+
+    public override List<TaxaServico> SelecionarTodos()
+    {
+        return dbSet
+            .Include(t => t.Empresa)
+            .ToList();
+    }
+}
