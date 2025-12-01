538899e6ccddc7a0b404241a3f253bdff5f508eb
Vicente Junior <vicente_pc@outlook.com>
2025-12-01 08:39:24 -0300

    add - Projeto da Camada de Apresentação AspNet MVC

---
+++ AutoManager.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AutoManager.WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
