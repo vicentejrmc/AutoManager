36a88c7e9b5a1ff9c936f51c99a1be70c96df2be
Vicente Junior <vicente_pc@outlook.com>
2025-12-04 10:33:07 -0300

    erro revert commit

---
+++ AutoManager.WebApp/Profiles/AutoManagerProfiles.cs
@@ -0,0 +1,24 @@
+﻿using AutoManager.WebApp.Models;
+using AutoMapper;
+
+namespace AutoManager.WebApp.Profiles;
+
+public class AutoManagerProfiles : Profile
+{
+    public AutoManagerProfiles()
+    {
+        CreateMap<LoginViewModel, LoginViewModel>();
+
+        //CreateMap<Funcionario, FuncionarioViewModel>().ReverseMap();
+
+        //CreateMap<Automovel, AutomovelViewModel>().ReverseMap();
+
+        //CreateMap<GrupoAutomovel, GrupoAutomovelViewModel>().ReverseMap();
+
+        //CreateMap<Cliente, ClienteViewModel>().ReverseMap();
+
+        //CreateMap<Usuario, UsuarioViewModel>().ReverseMap();
+
+        //CreateMap<Empresa, EmpresaViewModel>().ReverseMap();
+    }
+}
